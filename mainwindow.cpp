a9fa586562a69624624072ec824f7b715a5d3229
RamkumarMohan6383 <95962773+RamkumarMohan6383@users.noreply.github.com>
2025-03-15 16:51:15 +0530

    Updated On ( 15.03.2025 @ 4.51 PM)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,11 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    setWindowFlag(Qt::WindowCloseButtonHint,true);
+    setWindowTitle("Mouse Auto Click");
+    ui->pushButton->setDisabled(false);
+    ui->pushButton_2->setDisabled(true);
+    ui->pushButton_3->setDisabled(true);
 }
 
 MainWindow::~MainWindow() {
@@ -14,15 +19,18 @@ MainWindow::~MainWindow() {
 void MainWindow::on_pushButton_clicked()
 {
     //Mark
+
     QPoint pos = QCursor::pos();
     autoClicker.setClickPosition(pos.x(), pos.y());
+    ui->pushButton_2->setDisabled(false);
+    ui->pushButton_3->setDisabled(false);
     ui->statusbar->showMessage("Marked position: " + QString::number(pos.x()) + "," + QString::number(pos.y()));
 }
-
 
 void MainWindow::on_pushButton_2_clicked()
 {
    //Start
+    ui->pushButton->setDisabled(true);
     autoClicker.startClicking();
     ui->statusbar->showMessage("Auto-click started.");
 }
@@ -31,6 +39,7 @@ void MainWindow::on_pushButton_2_clicked()
 void MainWindow::on_pushButton_3_clicked()
 {
     //Stop
+    ui->pushButton->setDisabled(false);
     autoClicker.stopClicking();
     ui->statusbar->showMessage("Auto-click stopped.");
 }
